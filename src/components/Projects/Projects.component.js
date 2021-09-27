3cfaccbb201e6748b8d93dedf9a9935beed0b730
adiyy2001 <adiy@onet.pl>
2021-09-27 21:37:08 +0200

    add projects

--- src/components/Projects/Projects.component.js
+++ src/components/Projects/Projects.component.js
@@ -43,8 +43,8 @@ const Projects = () => (
               </TagList>
             </div>
             <UtilityList>
-              <ExternalLinks href={project.visit}>Code</ExternalLinks>
-              <ExternalLinks href={project.source}>Source</ExternalLinks>
+              <ExternalLinks href={project.visit} target="_blank">Visit</ExternalLinks>
+              {/* <ExternalLinks href={project.source}>Source</ExternalLinks> */}
             </UtilityList>
           </BlogCard>
         );
