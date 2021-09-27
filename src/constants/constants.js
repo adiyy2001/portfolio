102e51d25c17a8b35bc55db02078e97451b40242
adiyy2001 <adiy@onet.pl>
2021-09-27 21:24:23 +0200

    add projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,13 +2,32 @@ export const projects = [
   {
     title: "Netflix - clone",
     description:
-      "Using React & firebase, I've created copy of popular streaming service. Website allow you to create account, sign in and lookup for your favorite series and films",
+    "",
+      // "Using React & firebase, I've created copy of popular streaming service. Website allow you to create account, sign in and lookup for your favorite series and films",
     image: "/images/netflix.png",
     tags: ["React", "Styled-Components", "react-router-dom", "Fuse.js"],
-    source: "https://netflix-clone-f753f.web.app/browse",
-    visit: "https://github.com/adiyy2001/netflix-clone",
+    visit: "https://netflix-clone-f753f.web.app/browse",
+    // source: "https://github.com/adiyy2001/netflix-clone",
     id: 0,
   },
+  {
+    title: "Omani",
+    description:"",
+    image: "/images/omani.png",
+    tags: ["Html", "Css", "javascript", "Babel", "Animate.css"],
+    // source: "https://netflix-clone-f753f.web.app/browse",
+    visit: "https://omai-restaurant.de/",
+    id: 0,
+  },
+  {
+    title: "Dabov",
+    description:"",
+    image: "/images/dabov.png",
+    tags: [, "Wordpress", "PHP", "Html", "Css", "Woocomerce", "javascript"],
+    // source: "https://netflix-clone-f753f.web.app/browse",
+    visit: "https://dabov.coffee/",
+    id: 0,
+  }
 ];
 
 export const TimeLineData = [
